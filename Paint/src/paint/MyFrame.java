a50396981b343e2a6e9c1d17155d1ac7774f3938
shimaa4455 <shimaa.shehata6@yahoo.com>
2024-01-06 22:38:24 +0200

    update my frame class

--- Paint/src/paint/MyFrame.java
+++ Paint/src/paint/MyFrame.java
@@ -1,22 +1,34 @@
 package paint;
 
+import java.awt.BorderLayout;
 import javax.swing.*;
+import static javax.swing.WindowConstants.EXIT_ON_CLOSE;
 
 public class MyFrame extends JFrame {
 
-    private JPanel contentPanel;
-    private MyPanel myPanel;
+    private JToolBar paintControl;
+    private MyPanel paintPanel;
 
 
     public MyFrame() {
-        contentPanel = new JPanel();
-        myPanel = new MyPanel(this);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLocationRelativeTo(null);
-        setVisible(true);
+        paintPanel = new MyPanel(this);
+
+        paintControl = new PaintControl(this);
+        
+        add(paintPanel, BorderLayout.CENTER);
+
+        add(paintControl, BorderLayout.NORTH);
+
+        // set a default closer of window
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+
+     public JToolBar getPaintControl() {
+        return this.paintControl;
     }
 
     public MyPanel getPaintPanel() {
-        return myPanel;
+        return paintPanel;
     }
+
 }
